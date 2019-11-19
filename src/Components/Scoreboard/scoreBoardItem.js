1e3fa4626d84bfcc7e191d1de24ad617bbd7981b
Zxtch-arc <57876416+Zxtch-arc@users.noreply.github.com>
2019-11-19 16:51:56 +1100

    added styles and some w/l component

--- src/Components/Scoreboard/scoreBoardItem.js
+++ src/Components/Scoreboard/scoreBoardItem.js
@@ -5,7 +5,12 @@ import React from 'react';
     <div className="scoreBoardItem">
         <div className="teamLogo">
             <img src={src} alt='team logo'></img>
-        </div>
+       </div>
+        <div className="teamStats">
+                <span className="teamWins">1</span>
+                -
+                <span className="teamLosses">0</span>
+            </div>  
     </div>
   );
 }
