42f8789070bda2b1c3d8ad9b9565ca34f686806d
Tapia <MTapia@seven.com.au>
2019-11-21 12:10:31 +1100

    added base button

--- src/Components/CurrentMatch/currentMatch.js
+++ src/Components/CurrentMatch/currentMatch.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './currentMatch.css'
 
  const CurrentMatch = () => {
+
+    const handleClick = () => (('hey'));
   return (
       <div className='currentMatchContainer'>
          <span className='currentMatchTitle'>Current Match</span>
@@ -9,10 +11,13 @@ import './currentMatch.css'
             <div className='homeTeam'>
                 <img src='https://pbs.twimg.com/profile_images/1179402715392282627/AR4Xv02g_400x400.png'/> 
             </div>
-            <div className='currentScores'>
-                <span className='homeTeamscore'>3</span>
-                -
-                <span className='opposingTeamscore'>2</span>    
+            <div className='currentScoresContainer'>
+                <div className='currentScores'>
+                    <span className='homeTeamscore'>3</span>
+                    -
+                    <span className='opposingTeamscore'>2</span>    
+                </div>
+                <button onClick={handleClick}>Reveal Score</button>
             </div>
             <div className='opposingTeam'>
                 <img src='https://eui6dii703-flywheel.netdna-ssl.com/wp-content/uploads/2018/05/San-Francisco-Shock.png'/> 
