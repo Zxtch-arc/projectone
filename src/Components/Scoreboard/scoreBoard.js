943707810d959894b184e0f9905590c6da33a9c9
Zxtch-arc <57876416+Zxtch-arc@users.noreply.github.com>
2019-11-22 11:29:00 +1100

    workig api logo

--- src/Components/Scoreboard/scoreBoard.js
+++ src/Components/Scoreboard/scoreBoard.js
@@ -7,24 +7,39 @@ export default class ScoreBoard extends React.Component {
   constructor() {
     super();
     this.state = {
-      teamData: {}
+      team: {
+        name:'',
+        logo: '',
+      }
     };
   }
-  fetchData() {
+
+  fetchTeamName() {
     const OWL = new OverwatchLeague();
     return OWL.findTeamName(4410).then(response => {
-      this.setState({ teamData: response.data });
-      console.log(response.data);
+      this.setState({ team: {name: response.data }});
     });
   }
-  componentDidMount() {
-    this.fetchData();
+
+  
+  fetchTeamLogo() {
+    const OWL = new OverwatchLeague();
+    return OWL.getTeamLogo(4410).then(response => {
+      this.setState({ team: {logo: response.data }});
+    });
   }
+
+  componentDidMount() {
+    this.fetchTeamName();
+    this.fetchTeamLogo();
+  }
+
   render() {
     console.log("hey this is our awesome data", this.state);
+    const { team } = this.state;
     return (
       <div className="scoreBoard">
-        <ScoreBoardItem src="https://images.blz-contentstack.com/v3/assets/blt1ab304102f2340ec/bltd4784693a24686f9/5d637aa67c931b106e8afde5/Team_Logos_Text_Vancouver.svg?auto=webp" />
+        <ScoreBoardItem src={team.logo} />
       </div>
     );
   }
