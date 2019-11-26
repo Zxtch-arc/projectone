392c55295c40df2db3789779ddda1c22cdf97e1a
Tapia <MTapia@seven.com.au>
2019-11-26 11:17:11 +1100

    Updates

---
+++ precache-manifest.21dd1be2dc9b3144e4595d5692f7b39d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c19ed903890d15bfb7ca3c920c6a0a54",
+    "url": "/projectone/index.html"
+  },
+  {
+    "revision": "97aff07efcb71d4119d6",
+    "url": "/projectone/static/css/main.0fe7fb3e.chunk.css"
+  },
+  {
+    "revision": "15014ee33086323fb0ba",
+    "url": "/projectone/static/js/2.757c7b15.chunk.js"
+  },
+  {
+    "revision": "97aff07efcb71d4119d6",
+    "url": "/projectone/static/js/main.a3098c57.chunk.js"
+  },
+  {
+    "revision": "bb800ccce89418cc53d8",
+    "url": "/projectone/static/js/runtime-main.4e8d419f.js"
+  }
+]);
