eedf5f1474854a3750e53872527c1e2e665e08de
Tapia <MTapia@seven.com.au>
2019-11-26 15:32:22 +1100

    scoreboard items are generated still wip

--- src/Components/Scoreboard/scoreBoard.js
+++ src/Components/Scoreboard/scoreBoard.js
@@ -4,24 +4,59 @@ import OverwatchLeague from "overwatchleague";
 import ScoreBoardItem from "./scoreBoardItem";
 import "./scoreBoard.css";
 
+const OWL = new OverwatchLeague();
+
+const renderScoreBoardItem = (index, id, name, logo, primaryColor) => {
+  const primaryBackgroundColor = {
+    background: "black"
+  }
+
+  return(<ScoreBoardItem teamId={id} teamName={name} teamLogo={logo} key={index} style={primaryBackgroundColor} />);
+} 
 
 const ScoreBoard = () => {
-  const OWL = new OverwatchLeague();
 
-  // const [team, setTeam] = useState([]);
-
-  OWL.getVideos().then(response => {
-    const game = response.data
-    console.log(game)
-    // setTeam(game.competitors)
-  })
+  const [teams, setTeams] = useState([]);
   
-  // console.log(team)
+
+  const fetchData = () =>  {
+     OWL.getTeams().then(response => {
+      const data = response.data
+      console.log(data.competitors)
+
+      setTeams(data.competitors)
+    }).catch(error => console.log(error))
+  }
+
+
+  useEffect(() => {
+    fetchData();
+  });
+  
   return (
     <div className="scoreBoard">
-      <ScoreBoardItem teamId={7696}/>
+      {teams &&
+          teams.map(
+            (team, index) => {
+
+              const { id, name, logo, primaryColor } = team.competitor; 
+
+              console.log(id)
+              
+              return renderScoreBoardItem(index, id, name, logo, primaryColor);
+            },
+          )}
+
+
+    
     </div>
   );
 }
 
 export default ScoreBoard;
+
+
+  
+
+
+
--- src/Components/Scoreboard/scoreBoardItem.js
+++ src/Components/Scoreboard/scoreBoardItem.js
@@ -3,26 +3,20 @@ import OverwatchLeague from "overwatchleague";
 import Team from '../Team/team';
 import Match from '../Match/Match.js'
 
+const OWL = new OverwatchLeague();
 
-const ScoreBoardItem = ({teamId}) => {
- const OWL = new OverwatchLeague();
-  
- const [logo, setLogo] = useState('');
+const ScoreBoardItem = ({teamId, teamName, teamLogo}) => {
  const [wins, setMatchWins] = useState(0);
  const [loss, setLoss] = useState(0);
  const [opponentId, setOpponentId] = useState('');
  const [opponentLogo, setOpponentLogo] = useState('');
  const [opponentScore, setOpponentScore] = useState(0);
  const [homeScore, setHomeScore] = useState(0);
- const [teamName, setTeamName] = useState('');
 
- const fetchData = async () => {
-   const response = await  OWL.getTeamLogo(teamId)
-   .then(response => {
-     setLogo(response.data)
-   }).then(OWL.getMatchWins(teamId).then(response => {
+ const fetchData = () => {
+    OWL.getMatchWins(teamId).then(response => {
      setMatchWins(response.data)
-   })).then(OWL.getMatchLoss(teamId).then(response => { 
+   }).then(OWL.getMatchLoss(teamId).then(response => { 
      setLoss(response.data)
    })).then(OWL.lastMatchForTeam(teamId).then(response => {
      const team = response.data;
@@ -32,38 +26,30 @@ const ScoreBoardItem = ({teamId}) => {
   })).then(OWL.getTeamLogo(opponentId)
   .then(response => {
     setOpponentLogo(response.data)
-  }).then(OWL.findTeamName(teamId).then(response => {
-    setTeamName(response.data)
-  })).catch(error => console.log(error))
+  }).catch(error => console.log(error))
 )}
-//   .then(OWL.lastMatchForTeam(teamId).then(response => {
-//     const team = response.data;
-//     console.log('team test', team)
-//     setMatchscore(team.scores[0].id)
-//   .catch(error => console.log(error))
-//  }))
 
  useEffect(() => {
-   fetchData();
+   fetchData(teamId);
  })
 
  return(
   <div className="scoreBoardItem">
     <Team
       teamName={teamName}
-      logo={logo}
+      logo={teamLogo}
       wins={wins}
       teamLosses={loss}
     />
     <Match 
       matchTitle={'Recent'}
-      homeTeamLogo={logo} 
+      homeTeamLogo={teamLogo} 
       opposingTeamLogo={opponentLogo}
       homeTeamScore={homeScore}
       opposingTeamScore={opponentScore}
     />
 
-    <video className="videoPlayer" src="https://player2.majorleaguegaming.com/api/v2/player/embed/vod/owl-web?vid=SZEMePPMEzh&lang=en-us" controls width="250" crossOrigin="" >
+    <video className="videoPlayer" src="" controls width="250" crossOrigin="" >
     </video>
   </div>
 )};
